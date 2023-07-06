9523edbc8591a8b74f375038cce891bd0153af48
mikolyants1 <y.mikolyants@mail.ru>
2023-07-06 18:31:46 +0300

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -1,20 +1,8 @@
 import { useAppSelector } from '../store/store'
 import {Link } from 'react-router-dom'
-import React from 'react'
-interface state1{
-  text:Array<string>
-}
+
 export default function Header():JSX.Element {
     const current:string=useAppSelector((store)=>store.phone.current)
-    const [state,setState]=React.useState<state1>({text:[]})
-   /* React.useEffect(()=>{
-      for (let i = 0; i < current.length; i++) {
-       if (typeof current[i]==`string`) {
-        state.text.push(current[i])
-       }
-      }
-      setState({text:state.text})   
-    },[])*/
     return <div className='header' >    
              <div className='header1' >
                <div className='headerDiv'>
