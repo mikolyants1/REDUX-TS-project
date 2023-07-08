760edb7158122a34ff64c83fee897cf9e1d0f0cd
mikolyants1 <y.mikolyants@mail.ru>
2023-07-08 13:18:30 +0300

    Update main.tsx

--- components/main.tsx
+++ components/main.tsx
@@ -24,11 +24,11 @@ interface state3{
 }
 export default function Main():JSX.Element{
 const [ser,setSer]=React.useState<state2>({text:''})
-const [state,action]=React.useReducer(reducer,{show1:1,show2:0,show3:0,show4:0,show5:0})
+const [state,dispatch]=React.useReducer(reducer,{show1:1,show2:0,show3:0,show4:0,show5:0})
 const item6:Array<mass>=item1.concat(item2,item3,item4)
 const [items,setItems]=React.useState<state3>({item:item6})
-function reducer(state:state1,{type}:action):state1 {
-    switch (type) {
+function reducer(state:state1,action:action):state1 {
+    switch (action.type) {
     case 0:
         setSer({text:''})
         return {show1:1,show2:0,show3:0,show4:0,show5:0}
@@ -59,8 +59,8 @@ function reducer(state:state1,{type}:action):state1 {
 }
 
 function filter():void {
-    const val=ser.text.trim().toLocaleLowerCase()
-const list=item6.filter((item)=>{
+    const val:string=ser.text.trim().toLocaleLowerCase()
+const list:mass[]=item6.filter((item)=>{
     if (item.name.toLocaleLowerCase().indexOf(val)!==-1) {
         return item
     }
@@ -77,15 +77,15 @@ enum style{
 }
     return <div className='main'>
     <nav className='catalog'>
-<div className='cat1' onClick={()=>action({type:0})}>Mac</div>
-<div className='cat1' onClick={()=>action({type:1})}>Iphone</div>
-<div className='cat1' onClick={()=>action({type:2})}>Ipad</div>
-<div className='cat1' onClick={()=>action({type:3})}>Watch</div>
+<div className='cat1' onClick={()=>dispatch({type:0})}>Mac</div>
+<div className='cat1' onClick={()=>dispatch({type:1})}>Iphone</div>
+<div className='cat1' onClick={()=>dispatch({type:2})}>Ipad</div>
+<div className='cat1' onClick={()=>dispatch({type:3})}>Watch</div>
     </nav>
     <div className='ser'>
  <input type="text" style={style} value={ser.text}
   onChange={(e:React.ChangeEvent<HTMLInputElement>)=>setSer({text:e.target.value})} />
- <button className='serBut' onClick={()=>{action({type:4});filter()}}>search</button>
+ <button className='serBut' onClick={()=>{dispatch({type:4});filter()}}>search</button>
         </div>
     <div >
     <Mac show={state.show1} item={item1} />
