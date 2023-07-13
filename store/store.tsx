24423e7e3040e99f650fdf35f1462c2863984a92
mikolyants1 <y.mikolyants@mail.ru>
2023-07-13 14:25:46 +0300

    Update store.tsx

--- store/store.tsx
+++ store/store.tsx
@@ -1,22 +1,24 @@
-import { configureStore,combineReducers } from '@reduxjs/toolkit'
+import { configureStore,combineReducers, Reducer, AnyAction} from '@reduxjs/toolkit'
 import storage from 'redux-persist/lib/storage'
 import { persistStore, persistReducer, Persistor, WebStorage } from 'redux-persist'
 import slice from './slice'
 import slice1 from './slice1'
+import { ToolkitStore } from '@reduxjs/toolkit/dist/configureStore'
 interface state{
     key:string,
     storage:WebStorage
 }
+
 const config:state={
     key:'root',
     storage
 }
-const combine=combineReducers({
+const combine:Reducer=combineReducers({
     reduce:slice,
     phone:slice1
 })
-const persist=persistReducer(config,combine)
-const store=configureStore({
+const persist:Reducer<state,AnyAction>=persistReducer(config,combine)
+const store:ToolkitStore=configureStore({
     reducer:persist
 })
 export const cachedStore:Persistor=persistStore(store)
