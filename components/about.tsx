6cb00ef8cb3718fdb253f806cfa9d62fcad70f2d
mikolyants1 <y.mikolyants@mail.ru>
2023-07-25 22:37:34 +0300

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -1,28 +1,29 @@
 import React from 'react'
-import { useSearchParams } from 'react-router-dom'
+import { SetURLSearchParams,useSearchParams } from 'react-router-dom'
 import { add1 } from '../store/slice.js';
 import { useAppDispatch,useAppSelector } from '../types/state.js';
 import { item1,item2,item3,item4 } from './items.jsx';
 import { User} from '../store/slice.js';
 import { Link } from 'react-router-dom';
 import { AnyAction, Dispatch } from '@reduxjs/toolkit';
+import { URLSearchParams } from 'url';
 
 export default function About():JSX.Element {
    const [cless,setCless]=React.useState<string>('aboutImg')
-    const [searchParams] = useSearchParams();
-    const id:string=useAppSelector((store)=>store.phone.id)
-    const user:User[]=useAppSelector((store)=>store.reduce.user)
-    const user1:any=user.find((x)=>x.phone==id)
-    const dispatch:Dispatch<AnyAction>=useAppDispatch()
-    const name:any=searchParams.get("name")
-    const item:any|undefined=item1.concat(item2,item3,item4).find((x)=>x.name==name)
-    const src:string|undefined=item.src
-    const price:string|undefined=item.price
+   const [searchParams]:[URLSearchParams,SetURLSearchParams] = useSearchParams();
+   const id:string=useAppSelector((store)=>store.phone.id)
+   const user:User[]=useAppSelector((store)=>store.reduce.user)
+   const user1:any=user.find((x)=>x.phone==id)
+   const dispatch:Dispatch<AnyAction>=useAppDispatch()
+   const name:any=searchParams.get("name")
+   const item:any=item1.concat(item2,item3,item4).find((x)=>x.name==name)
+   const src:string|undefined=item.src
+   const price:string|undefined=item.price
     React.useEffect(()=>{
  if (item1.some((x)=>x.name==name)) {
    setCless('aboutImgMac')
  }
-    },[])
+  },[])
      return <div style={{width:'100%'}}>
         <div className='aboutDiv'>
       <img className={cless}  src={`${src}`} alt="" />
