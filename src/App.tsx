64388646d8d659e10375702004a9e84108927d4e
mikolyants1 <y.mikolyants@mail.ru>
2023-08-02 15:34:44 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Entry from './components/entry'
 import About from './components/about'
 import Main from './components/main'
 import Header from './components/header'
-
+import React from 'react'
 const Home:React.FC=():JSX.Element=> {
    return <div>
        <Outlet />
@@ -42,23 +42,49 @@ const BaskList:React.FC=():JSX.Element=>{
       <Bask2 />
     </div>
  }
-
+const Loading:React.FC=():JSX.Element=>{
+  const load=React.useRef<HTMLDivElement>(null!)
+  enum style {
+    width='100%',
+    textAlign='center'
+  }
+  React.useEffect(()=>{
+     setInterval(() => {
+        setTimeout(() => {
+            load.current.innerHTML=''
+        }, 0);
+        setTimeout(() => {
+            load.current.innerHTML='.'
+        }, 200);
+        setTimeout(() => {
+            load.current.innerHTML='..'
+        }, 400);
+        setTimeout(() => {
+            load.current.innerHTML='...'
+        }, 600);
+    }, 1000);
+  })
+     return <div style={style}>
+        <div>Loading</div>
+        <div ref={load}></div>
+     </div>
+}
 export default function App():JSX.Element{
   return <Provider store={store}>
-     <PersistGate persistor={cachedStore} >
-        <Router>
-          <Routes>
-            <Route path='/' element={<Home />}>
-              <Route index element={<Links />} />
-              <Route path='regist' element={<Regist />} />
-              <Route path='home' element={<Shop />} >
-              <Route index element={<ShopList />} />
-            <Route path='about' element={<Names />} />
-            <Route path='bask' element={<BaskList />} />
-            </Route>
-          </Route>
-        </Routes>
-      </Router>
-    </PersistGate>
-  </Provider>
+           <PersistGate persistor={cachedStore} loading={<Loading />} >
+             <Router>
+               <Routes>
+                 <Route path='/' element={<Home />}>
+                  <Route index element={<Links />} />
+                   <Route path='regist' element={<Regist />} />
+                   <Route path='home' element={<Shop />} >
+                    <Route index element={<ShopList />} />
+                    <Route path='about' element={<Names />} />
+                    <Route path='bask' element={<BaskList />} />
+                 </Route>
+               </Route>
+             </Routes>
+           </Router>
+         </PersistGate>
+       </Provider>
 }
