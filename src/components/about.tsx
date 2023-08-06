40f4d58595a9f2b8abb0b0a1a28930cf4b771ebe
mikolyants1 <y.mikolyants@mail.ru>
2023-08-06 14:12:54 +0300

    d

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -7,31 +7,32 @@ import { User} from '../store/slice.js';
 import { Link } from 'react-router-dom';
 import { AnyAction, Dispatch } from '@reduxjs/toolkit';
 import { URLSearchParams } from 'url';
+import { mass } from './main.js';
 
 export default function About():JSX.Element {
    const [cless,setCless]=React.useState<string>('aboutImg')
    const [searchParams]:[URLSearchParams,SetURLSearchParams] = useSearchParams();
    const id:string=useAppSelector((store)=>store.phone.id)
    const user:User[]=useAppSelector((store)=>store.reduce.user)
-   const user1:any=user.find((x)=>x.phone==id)
+   const user1:any=user.find(({phone}:User)=>phone==id)
    const dispatch:Dispatch<AnyAction>=useAppDispatch()
-   const name:any=searchParams.get("name")
-   const item:any=item1.concat(item2,item3,item4).find((x)=>x.name==name)
+   const Name:any=searchParams.get("name")
+   const item:any=item1.concat(item2,item3,item4).find(({name}:mass)=>name==Name)
    const src:string|undefined=item.src
    const price:string|undefined=item.price
     React.useEffect(()=>{
- if (item1.some((x)=>x.name==name)) {
+ if (item1.some(({name}:mass)=>name==Name)) {
    setCless('aboutImgMac')
  }
   },[])
      return <div style={{width:'100%'}}>
         <div className='aboutDiv'>
       <img className={cless}  src={`${src}`} alt="" />
-      <div className='aboutName'>{name}</div>  
+      <div className='aboutName'>{Name}</div>  
       <div className='aboutPrice' >{price}p</div>  
      <div className='divBut'>
      <button className='aboutBut'
-      onClick={()=>{dispatch(add1({id:user1.id,name:name,price:price,src:src}))}} >добавить в корзину
+      onClick={()=>{dispatch(add1({id:user1.id,name:Name,price:price,src:src}))}} >добавить в корзину
       </button>
      </div>
      <div className='baskBack'>
--- src/components/bask.tsx
+++ src/components/bask.tsx
@@ -5,27 +5,28 @@ import { useAppDispatch,useAppSelector } from '../types/state.js'
 import { item1 } from './items.jsx'
 import { User,bask } from '../store/slice'
 import { AnyAction, Dispatch } from '@reduxjs/toolkit'
+import { mass } from './main.js'
 export default function Bask2():JSX.Element {
     const id:string|undefined=useAppSelector((store)=>store.phone.id)
     const user:User[]=useAppSelector((store)=>store.reduce.user)
     const dispatch:Dispatch<AnyAction>=useAppDispatch()
-     const user1:any=user.find((x)=>x.phone==id)
+     const user1:any=user.find(({phone}:User)=>phone==id)
      let [imgClass,divClass]:string[]=['','']
     const list:JSX.Element[]=user1.bask.map(({name,price,src}:bask,index:number)=>{
- if (item1.some((x)=>x.name==name)) {
+ if (item1.some(({name:n}:mass)=>n==name)) {
    imgClass='baskImgMac'
     divClass='item2'
 }else{
     imgClass='baskImg'
       divClass='item1'
 }
-           return <div key={index} className={divClass}>
-              <img className={imgClass}  src={`../${src}`} alt="" />
-              <div className='baskName' >{name}</div>
-              <div className='baskPrice'>{price}</div>
-              <div style={{width:'80%',margin:'auto'}}><button style={{width:'50%'}}>купить</button>
-              <button style={{width:'50%'}} onClick={()=>{dispatch(del({id:user1.id,index:index}))}}>удалить</button></div>
-            </div>
+   return <div key={index} className={divClass}>
+          <img className={imgClass}  src={`../${src}`} alt="" />
+        <div className='baskName' >{name}</div>
+        <div className='baskPrice'>{price}</div>
+        <div style={{width:'80%',margin:'auto'}}><button style={{width:'50%'}}>купить</button>
+        <button style={{width:'50%'}} onClick={()=>{dispatch(del({id:user1.id,index:index}))}}>удалить</button></div>
+      </div>
        })
       
        if (user1.bask.length==0) {
